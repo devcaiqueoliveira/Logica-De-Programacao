677626899b3885f7f144d81a2579166b09aa48f3
Caique Oliveira <devcaiqueoliveira@gmail.com>
2025-02-11 16:47:50 -0300

    vetores4

--- Vetores4/src/Main.java
+++ Vetores4/src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        double[] n = new double[x];
+        double soma = 0;
+        double cont = 0;
+        double media = 0;
+
+        for (int i = 0; i < x; i++) {
+            n[i] = sc.nextDouble();
+            soma += n[i];
+            cont = i + 1;
+        }
+
+        media =  soma / cont;
+        System.out.println(media);
+
+        for (int i = 0; i < x; i++) {
+            if (n[i] < media) {
+                System.out.println(n[i]);
+            }
+        }
+
+        sc.close();
     }
 }
